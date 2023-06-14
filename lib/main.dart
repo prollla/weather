525887bbbc4ca433d9395ef03f9244bee3903775
Firsov <skull.boy@rambler.ru>
2023-06-14 19:04:42 +0300

    1234

--- lib/main.dart
+++ lib/main.dart
@@ -18,60 +18,192 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page',),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class A {
+  A({required this.id, this.name,});
+  final int id;
+  final String? name;
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  List<A> items = [
+    A(id: 123, name: 'test1'),
+    A(id: 321, name: 'test2'),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    double borderRadius = MediaQuery.of(context).size.width * 0.12;
+
     return Scaffold(
         body: CustomScrollView(
       slivers: [
         SliverAppBar(
           expandedHeight: 200,
           // floating: true,
-          pinned: false,
-          flexibleSpace: Container(
-            height: 200,
-          ),
+          pinned: false,flexibleSpace: Stack(
+          children: [
+            Positioned.fill(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(child: Image.asset('assets/Облачно.png')),
+                    Text('Облачно'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Max: 29°'),
+                        SizedBox(width: 10),
+                        Text('Min: 15°'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
         ),
         SliverList(
-            delegate:
-                SliverChildBuilderDelegate(childCount: 99, (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Row(
-              children: [
-                const Spacer(),
-                Expanded(
-                  flex: 8,
+          delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Card(
-                    child: ListTile(
-                      title:
-                          Text('Карточка $index', textAlign: TextAlign.center),
-                      subtitle: Text(
-                        'Описание карточки $index',
-                        textAlign: TextAlign.center,
-                      ),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                    ),
+                    child: LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+                        return Row(
+                          children: [
+                            SizedBox(
+                              height: constraints.maxWidth * 0.1, // 10% от ширины экрана
+                              width: constraints.maxWidth * 0.1, // 10% от ширины экрана
+                              child: Image.asset('assets/Влажность.png'),
+                            ),
+                            Text('123'),
+                            Spacer(flex: 1,),
+                            SizedBox(
+                              height: constraints.maxWidth * 0.1, // 10% от ширины экрана
+                              width: constraints.maxWidth * 0.1, // 10% от ширины экрана
+                              child: Image.asset('assets/Градусник.png'),
+                            ),
+                            Text('90%'),
+                            Spacer(flex:1),
+                            SizedBox(
+                              height: constraints.maxWidth * 0.1, // 10% от ширины экрана
+                              width: constraints.maxWidth * 0.1, // 10% от ширины экрана
+                              child: Image.asset('assets/Ветер.png'),
+                            ),
+                            Text('19 km/h')
+                          ],
+                        );
+                      },
                     ),
                   ),
                 ),
-                const Spacer(),
-              ],
-            ),
-          );
-        }))
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                    ),
+                    child: Column(
+                      children: [
+                        // Верхняя строка с двумя элементами
+                        Row(
+                          children: [
+                            Expanded(child: Center(child: Text('Today'))),
+                            Expanded(child: Center(child: Text('June 14'))),
+                          ],
+                        ),
+
+                        // Остальные строки с четырьмя элементами в каждой
+                        Row(
+                          children: [
+                            Expanded(child: Center(child: Text('29°'))),
+                            Expanded(child: Center(child: Text('29°'))),
+                            Expanded(child: Center(child: Text('29°'))),
+                            Expanded(child: Center(child: Text('29°'))),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                        Expanded(child: FractionallySizedBox(
+                        widthFactor: 0.45,  // Устанавливаем ширину изображения в 50% от ширины родительского контейнера
+                          child: Image.asset('assets/Облачно.png'),
+                        )),
+                            Expanded(child: FractionallySizedBox(
+                              widthFactor: 0.45,  // Устанавливаем ширину изображения в 50% от ширины родительского контейнера
+                              child: Image.asset('assets/Облачно.png'),
+                            )),
+                            Expanded(child: FractionallySizedBox(
+                              widthFactor: 0.45,  // Устанавливаем ширину изображения в 50% от ширины родительского контейнера
+                              child: Image.asset('assets/Облачно.png'),
+                            )),
+                            Expanded(child: FractionallySizedBox(
+                              widthFactor: 0.45,  // Устанавливаем ширину изображения в 50% от ширины родительского контейнера
+                              child: Image.asset('assets/Облачно.png'),
+                            )),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(child: Center(child: Text('00:00'))),
+                            Expanded(child: Center(child: Text('01:00'))),
+                            Expanded(child: Center(child: Text('02:00'))),
+                            Expanded(child: Center(child: Text('03:00 '))),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                    ),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.3, // 30% от высоты экрана
+                      width: MediaQuery.of(context).size.width * 0.7, // 80% от ширины экрана
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                    ),
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.3, // 30% от высоты экрана
+                      width: MediaQuery.of(context).size.width * 0.7, // 80% от ширины экрана
+                    ),
+                  ),
+                ),
+              ]
+          ),
+        )
       ],
     ));
   }
